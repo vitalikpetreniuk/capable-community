1e8237c4cf2c0c788c3c2314f9869390ee36d639
vadimvynnchuk <vadimvinnicuk4@gmail.com>
2023-03-09 15:15:32 +0200

    add sliders

--- _dist/js/scripts.js
+++ _dist/js/scripts.js
@@ -3,8 +3,7 @@
 // 	$('.menu').toggleClass('_active');
 // 	$('body').toggleClass('_lock');
 // })
-
-
+// import Swiper, { Pagination } from 'swiper';
 
 //scroll header
 
@@ -26,6 +25,130 @@ $('.btns-wrap > .btn-link').hover(
 		$(this).addClass('btn-link-active');
 	}
 );
+
+
+
+//swiper review
+
+var swiperReviews = new Swiper(".swiperReviews", {
+    loop: true,
+    speed: 1200,
+		slidesPerView: 1.7,
+		spaceBetween: 30,
+		// pagination: {
+		//   el: ".swiper-pagination",
+		//   clickable: true,
+		  
+		// },
+
+		breakpoints: {
+            
+            // 320: {
+            //   slidesPerView: 2.05,
+            //   spaceBetween: 10,
+            // },
+          
+            // 480: {
+            //   slidesPerView: 2.03,
+            //   spaceBetween: 10,
+            // },
+    
+            // 760: {
+            //   slidesPerView: 2,
+            //   spaceBetween: 30,
+            // },
+
+            // 1100: {
+            //   slidesPerView: 3,
+            //   spaceBetween: 30,
+            // },
+
+
+
+        }
+	});
+
+
+
+
+	var swiperNews = new Swiper(".swiperNews", {
+		loop: true,
+		speed: 1200,
+			slidesPerView: 4,
+			spaceBetween: 30,
+			pagination: {
+			  el: ".swiper-pagination-news",
+			  clickable: true,
+			  
+			},
+	
+			breakpoints: {
+				
+				// 320: {
+				//   slidesPerView: 2.05,
+				//   spaceBetween: 10,
+				// },
+			  
+				// 480: {
+				//   slidesPerView: 2.03,
+				//   spaceBetween: 10,
+				// },
+		
+				// 760: {
+				//   slidesPerView: 2,
+				//   spaceBetween: 30,
+				// },
+	
+				// 1100: {
+				//   slidesPerView: 3,
+				//   spaceBetween: 30,
+				// },
+	
+	
+	
+			}
+	});
+
+
+
+	var swiperBlogs = new Swiper(".swiperBlogs", {
+		loop: true,
+		speed: 1200,
+			slidesPerView: 3,
+			spaceBetween: 30,
+			pagination: {
+			  el: ".swiper-pagination-blogs",
+			  clickable: true,
+			  
+			},
+	
+			breakpoints: {
+				
+				// 320: {
+				//   slidesPerView: 2.05,
+				//   spaceBetween: 10,
+				// },
+			  
+				// 480: {
+				//   slidesPerView: 2.03,
+				//   spaceBetween: 10,
+				// },
+		
+				// 760: {
+				//   slidesPerView: 2,
+				//   spaceBetween: 30,
+				// },
+	
+				// 1100: {
+				//   slidesPerView: 3,
+				//   spaceBetween: 30,
+				// },
+	
+	
+	
+			}
+	});
+
 
 
 
